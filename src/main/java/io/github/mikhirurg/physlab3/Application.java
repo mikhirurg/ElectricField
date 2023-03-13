6596f28708bc58edfd9fa196deacd0bbfdcf648c
mikhirurg <mikhail.a.ushakov@gmail.com>
2023-03-13 23:22:44 +0100

    Language bundle fix

--- src/main/java/io/github/mikhirurg/physlab3/Application.java
+++ src/main/java/io/github/mikhirurg/physlab3/Application.java
@@ -1,6 +1,7 @@
 package io.github.mikhirurg.physlab3;
 
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
@@ -9,8 +10,14 @@ public class Application {
     private static final ResourceBundle resourceBundleUS;
 
     static {
-        resourceBundle = PropertyResourceBundle.getBundle("AppBundle");
-        resourceBundleUS = PropertyResourceBundle.getBundle("AppBundle", Locale.forLanguageTag("en-us"));
+        ResourceBundle resourceBundleTmp;
+        try {
+            resourceBundleTmp = PropertyResourceBundle.getBundle("AppBundle");
+        } catch (MissingResourceException e) {
+            resourceBundleTmp = PropertyResourceBundle.getBundle("AppBundle", Locale.forLanguageTag("en"));
+        }
+        resourceBundle = resourceBundleTmp;
+        resourceBundleUS = PropertyResourceBundle.getBundle("AppBundle", Locale.forLanguageTag("en"));
     }
 
     public static String getString(String key) {
